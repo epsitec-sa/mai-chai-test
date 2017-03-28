0cba867eae32593d4a6e04aa4a7e66644eea547b
Pierre Arnaud <arnaud@epsitec.ch>
2017-03-28 09:18:27 +0200

    Cosmetic: remove warnings and layout code

--- src.test/index.js
+++ src.test/index.js
@@ -1,15 +1,17 @@
-'use strict';
+/* global describe it */
 
 import Foo from '../src/index.js';
-
 import {expect} from 'mai-chai';
 
+/******************************************************************************/
 
 describe ('Foo class', () => {
   it ('returns 42', () => {
     expect (Foo.answer ()).to.equal (42);
-  })
+  });
   it ('returns a number', () => {
     expect (typeof Foo.answer ()).to.equal ('number');
-  })
+  });
 });
+
+/******************************************************************************/
--- src/index.js
+++ src/index.js
@@ -5,4 +5,3 @@ export default class Foo {
     return 42;
   }
 }
- 
